349af6898a526dd182b49cb0853c465bc5f9446e
M. Najamudin Ridha <m@najamudinridha.com>
2026-06-09 15:34:14 +0800

    create index.php

---
+++ index.php
@@ -0,0 +1,23 @@
+<?php
+for ($i = 1; $i <= 10; $i++) {
+  echo "Hello World " . $i . " : ";
+  echo "<input name='hello$i' type='text'/> : ";
+  echo "<input name='date$i' type='date'/> : ";
+  echo "<input name='color$i' type='color'/> : ";
+  echo "<select name='select$i'>
+    <option>Laki-laki</option>
+    <option>Perempuan</option>
+  </select><br>";
+}
+
+$con = mysqli_connect('localhost', 'root', '', 'teknologiinformasi');
+$dosen = mysqli_query($con, "SELECT * FROM dosen");
+
+echo "<br>";
+echo "<table style='border: 1px solid black'>";
+echo "<tr><th>NIP</th><th>Nama Dosen</th></tr>";
+while ($data = mysqli_fetch_array($dosen)) {
+  echo "<tr><td>" . $data['nip'] . "</td><td>" . $data['nama'] . "</td></tr>";
+}
+echo "</table>";
+?>
